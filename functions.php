f52f44068c60c608f176d982958db4cc1f21c6c4
Lee Garner <lee@leegarner.com>
2021-08-20 14:45:08 -0700

    Cleanup formatting, fix block widths

--- functions.php
+++ functions.php
@@ -1,14 +1,10 @@
 <?php
 /**
-* @package  glFusion CMS
-*
-* @license GNU General Public License version 2 or later
-*     http://www.opensource.org/licenses/gpl-license.php
-*
-*  Copyright (C) 2010-2017 by the following authors:
-*   Mark R. Evans   mark AT glfusion DOT org
-*
-*/
+ * Theme functions for the UIKit v3 base theme.
+ * @package     glFusion CMS
+ * @license     GNU General Public License version 2 or later
+ *              http://www.opensource.org/licenses/gpl-license.php
+ */
 
 // this file can't be used on its own
 if (!defined ('GVERSION')) {
@@ -24,7 +20,6 @@ $_SYSTEM['disable_jquery_menu'] = true;     // not needed for this theme
 $_SYSTEM['disable_jquery_slimbox'] = true;  // use uikit
 
 // multiple language support
-
 /*
 $lang = COM_getLanguageId();
 if (empty($lang)) {
@@ -44,7 +39,6 @@ for ($i = 0; $i < $nrows; $i++) {
 */
 
 // custom block templates
-
 $_BLOCK_TEMPLATE['_admin_block'] = 'blockheader-admin.thtml,blockfooter-admin.thtml';
 $_BLOCK_TEMPLATE['whats_related_block'] = 'blockheader-related.thtml,blockfooter-related.thtml';
 $_BLOCK_TEMPLATE['story_options_block'] = 'blockheader-related.thtml,blockfooter-related.thtml';
@@ -55,12 +49,12 @@ $_BLOCK_TEMPLATE['topicoption'] = 'topicoption.thtml,topicoption.thtml';
 $_BLOCK_TEMPLATE['_submit_story'] = 'blockheader-submitstory.thtml,blockfooter-submitstory.thtml';
 $_BLOCK_TEMPLATE['rss_feeds'] = 'blockheader-rss.thtml,blockfooter-right.thtml';
 
-/*
+/**
  * Full_content - no left / right
  * left_content  - left nav ONLY no RIGHT nav
  * left_content_right - both navs
  * content_right - right nav ONLY
-*/
+ */
 $uiStyles = array(
     'full_content' => array(
         'left_class' => '',
@@ -68,13 +62,13 @@ $uiStyles = array(
         'right_class' => '',
     ),
     'left_content' => array(
-        'left_class' => 'uk-width-1-4@m',
-        'content_class' => 'uk-width-3-4@m',
+        'left_class' => 'uk-width-1-4@m uk-width-1-5@l',
+        'content_class' => 'uk-width-3-4@m uk-width4-5@l',
         'right_class' => '',
     ),
     'left_content_right' => array(
-        'left_class' => 'uk-width-1-4@m',
-        'content_class' => 'uk-width-3-4@m',
+        'left_class' => 'uk-width-1-4@m uk-width-1-5@l',
+        'content_class' => 'uk-width-3-4@m uk-width4-5@l',
         'right_class' => '',
     ),
     'content_right' => array(
@@ -100,15 +94,15 @@ define('BLOCK_LOCATION_OTHER',  3);  // other
 
 $blockInterface = array(
     'left'  => array(                        // BLOCK_LOCATION_NAV
-                'enabled'   => true,
-                'location'  => 'right',      // left, right, footer, other
-                'title'     => $LANG01['blocks_right_title']
-                ),
+        'enabled'   => true,
+        'location'  => 'right',      // left, right, footer, other
+        'title'     => $LANG01['blocks_right_title']
+    ),
     'right' => array(                       // BLOCK_LOCATION_EXTRA
-                'enabled'   => true,
-                'location'  => 'footer',     // left, right, footer, other
-                'title'     => $LANG01['blocks_footer_title']
-                )
+        'enabled'   => true,
+        'location'  => 'footer',     // left, right, footer, other
+        'title'     => $LANG01['blocks_footer_title']
+    ),
 );
 
 // define the JS we need for this theme..
@@ -117,7 +111,6 @@ $outputHandle = outputHandler::getInstance();
 // if the theme needs jquery-ui - uncomment
 // also see jquery-ui styles below..
 $outputHandle->addScriptFile($_CONF['path_html'].'javascript/jquery/jquery-ui.min.js');
-
 $outputHandle->addScriptFile($_CONF['path_html'].'javascript/ps.js');
 $outputHandle->addScriptFile($_CONF['path_layout'].'js/jquery.smartmenus.min.js');
 $outputHandle->addScriptFile($_CONF['path_layout'].'js/glfusion.js');
@@ -128,62 +121,11 @@ $outputHandle->addCSSFile($_CONF['path_layout'].'css/uk2-compat.css',HEADER_PRIO
 $outputHandle->addCSSFile($_CONF['path_layout'].'css/uk2-icon.css',HEADER_PRIO_VERYHIGH);
 $outputHandle->addCSSFile($_CONF['path_layout'].'css/uk2-width.css',HEADER_PRIO_VERYHIGH);
 
-
-$outputHandle->addScriptFile($_CONF['path_layout'].'components/datetimepicker/jquery.datetimepicker.full.min.js');
-$outputHandle->addCSSFile($_CONF['path_layout'].'components/datetimepicker/jquery.datetimepicker.min.css',HEADER_PRIO_VERYHIGH);
-
-//
-/*$outputHandle->addCSSFile($_CONF['path_layout'].'css/components/accordion'.$styleType.'min.css',HEADER_PRIO_VERYHIGH);
-$outputHandle->addCSSFile($_CONF['path_layout'].'css/components/autocomplete'.$styleType.'min.css',HEADER_PRIO_VERYHIGH);
-$outputHandle->addCSSFile($_CONF['path_layout'].'css/components/datepicker'.$styleType.'min.css',HEADER_PRIO_VERYHIGH);
-$outputHandle->addCSSFile($_CONF['path_layout'].'css/components/dotnav'.$styleType.'min.css',HEADER_PRIO_VERYHIGH);
-$outputHandle->addCSSFile($_CONF['path_layout'].'css/components/form-advanced'.$styleType.'min.css',HEADER_PRIO_VERYHIGH);
-$outputHandle->addCSSFile($_CONF['path_layout'].'css/components/form-file'.$styleType.'min.css',HEADER_PRIO_VERYHIGH);
-$outputHandle->addCSSFile($_CONF['path_layout'].'css/components/form-password'.$styleType.'min.css',HEADER_PRIO_VERYHIGH);
-$outputHandle->addCSSFile($_CONF['path_layout'].'css/components/form-select'.$styleType.'min.css',HEADER_PRIO_VERYHIGH);
-$outputHandle->addCSSFile($_CONF['path_layout'].'css/components/htmleditor'.$styleType.'min.css',HEADER_PRIO_VERYHIGH);
-$outputHandle->addCSSFile($_CONF['path_layout'].'css/components/nestable'.$styleType.'min.css',HEADER_PRIO_VERYHIGH);
-$outputHandle->addCSSFile($_CONF['path_layout'].'css/components/notify'.$styleType.'min.css',HEADER_PRIO_VERYHIGH);
-$outputHandle->addCSSFile($_CONF['path_layout'].'css/components/placeholder'.$styleType.'min.css',HEADER_PRIO_VERYHIGH);
-$outputHandle->addCSSFile($_CONF['path_layout'].'css/components/progress'.$styleType.'min.css',HEADER_PRIO_VERYHIGH);
-$outputHandle->addCSSFile($_CONF['path_layout'].'css/components/search'.$styleType.'min.css',HEADER_PRIO_VERYHIGH);
-$outputHandle->addCSSFile($_CONF['path_layout'].'css/components/slidenav'.$styleType.'min.css',HEADER_PRIO_VERYHIGH);
-$outputHandle->addCSSFile($_CONF['path_layout'].'css/components/slider'.$styleType.'min.css',HEADER_PRIO_VERYHIGH);
-$outputHandle->addCSSFile($_CONF['path_layout'].'css/components/slideshow'.$styleType.'min.css',HEADER_PRIO_VERYHIGH);
-$outputHandle->addCSSFile($_CONF['path_layout'].'css/components/sortable'.$styleType.'min.css',HEADER_PRIO_VERYHIGH);
-$outputHandle->addCSSFile($_CONF['path_layout'].'css/components/sticky'.$styleType.'min.css',HEADER_PRIO_VERYHIGH);
-$outputHandle->addCSSFile($_CONF['path_layout'].'css/components/tooltip'.$styleType.'min.css',HEADER_PRIO_VERYHIGH);
-$outputHandle->addCSSFile($_CONF['path_layout'].'css/components/upload'.$styleType.'min.css',HEADER_PRIO_VERYHIGH);
- */
-
 // Load our JS specific to this theme
 //$outputHandle->addScriptFile($_CONF['path_layout'].'js/uikit.min.js');
 $outputHandle->addScriptFile($_CONF['path_layout'].'js/uikit.js');
 $outputHandle->addScriptFile($_CONF['path_layout'].'js/uikit-icons.min.js');
 
-// optional UIKIT components
-/*$outputHandle->addScriptFile($_CONF['path_layout'].'js/components/accordion.min.js');
-$outputHandle->addScriptFile($_CONF['path_layout'].'js/components/autocomplete.min.js');
-$outputHandle->addScriptFile($_CONF['path_layout'].'js/components/datepicker.min.js');
-$outputHandle->addScriptFile($_CONF['path_layout'].'js/components/form-password.min.js');
-$outputHandle->addScriptFile($_CONF['path_layout'].'js/components/form-select.min.js');
-$outputHandle->addScriptFile($_CONF['path_layout'].'js/components/grid.min.js');
-$outputHandle->addScriptFile($_CONF['path_layout'].'js/components/htmleditor.min.js');
-$outputHandle->addScriptFile($_CONF['path_layout'].'js/components/lightbox.min.js');
-$outputHandle->addScriptFile($_CONF['path_layout'].'js/components/nestable.min.js');
-$outputHandle->addScriptFile($_CONF['path_layout'].'js/components/notify.min.js');
-$outputHandle->addScriptFile($_CONF['path_layout'].'js/components/pagination.min.js');
-$outputHandle->addScriptFile($_CONF['path_layout'].'js/components/parallax.min.js');
-$outputHandle->addScriptFile($_CONF['path_layout'].'js/components/search.min.js');
-$outputHandle->addScriptFile($_CONF['path_layout'].'js/components/slider.min.js');
-$outputHandle->addScriptFile($_CONF['path_layout'].'js/components/slideset.min.js');
-$outputHandle->addScriptFile($_CONF['path_layout'].'js/components/slideshow.min.js');
-$outputHandle->addScriptFile($_CONF['path_layout'].'js/components/sortable.min.js');
-$outputHandle->addScriptFile($_CONF['path_layout'].'js/components/sticky.min.js');
-$outputHandle->addScriptFile($_CONF['path_layout'].'js/components/timepicker.min.js');
-$outputHandle->addScriptFile($_CONF['path_layout'].'js/components/tooltip.min.js');
-$outputHandle->addScriptFile($_CONF['path_layout'].'js/components/upload.min.js');
- */
 // Media Player
 $outputHandle->addScriptFile($_CONF['path_html'].'javascript/addons/mediaplayer/mediaelement-and-player.min.js');
 $outputHandle->addCSSFile($_CONF['path_html'] .'javascript/addons/mediaplayer/mediaelementplayer.css');
@@ -197,11 +139,10 @@ if ( file_exists($_CONF['path_layout'] .'custom.css') ) {
 }
 
 
-/*
+/**
  * return the class to use for tooltips
  */
 function theme_getToolTipStyle()
 {
     return('tooltip');
 }
-?>
